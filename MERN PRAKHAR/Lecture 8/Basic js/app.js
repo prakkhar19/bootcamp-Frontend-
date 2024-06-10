5704698044fb398a988df937a176d4bb09c1abcc
Prakhar <133490820+prakkhar19@users.noreply.github.com>
2024-06-10 16:07:44 +0530

    Add files via upload

---
+++ MERN PRAKHAR/Lecture 8/Basic js/app.js
@@ -0,0 +1,71 @@
+console.log("js is working");
+
+//string
+
+let myFirstName = 'prakhar';
+let myLastName = 'srivastava';
+
+console.log(myFirstName);
+console.log(myLastName);
+
+let space = " "
+
+
+console.log(myFirstName + myLastName);
+
+console.log(myFirstName + space + myLastName);
+
+console.log(myFirstName + " " + myLastName);
+
+console.log(`my first name is ${myFirstName} and my last name is ${myLastName}`);
+
+console.log(myFirstName.length)
+
+console.log(myLastName.toUpperCase());
+
+// let naam = 'SHRESTH'
+// console.log(naam.toLowerCase())
+
+let naam = '        shresth          '
+console.log(naam.trim())
+console.log(typeof(naam))
+
+
+// number
+
+let myNumber = 12345
+console.log(myNumber)
+
+let deicamlNumber = 23.3456
+console.log(deicamlNumber)
+
+let negNumber = -234
+console.log(negNumber)
+console.log(typeof(negNumber))
+
+let sum = 1+567
+console.log(sum)
+
+//math function
+
+console.log(Math.PI)
+console.log(Math.sqrt(625))
+console.log(Math.max(10, 13, 23, 45))
+console.log(Math.ceil(10.86))
+console.log(Math.floor(10.86))
+
+console.log(Math.random()*20)
+
+console.log(Math.floor(Math.random()*10))
+
+//boolean
+
+let myNaam = true
+console.log(myNaam)
+console.log(typeof(myNaam))
+
+let one = 1
+
+console.log(myNaam == one)
+console.log(myNaam === one)
+
