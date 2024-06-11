9c7077a899fabfb088f7c8a350ed0a0f5754330b
Prakhar <133490820+prakkhar19@users.noreply.github.com>
2024-06-11 13:05:02 +0530

    Add files via upload

---
+++ MERN PRAKHAR/Lecture 8/Basic js/define js/app.js
@@ -0,0 +1,17 @@
+//weekly typed
+
+let naam = "prakhar";
+naam = "mayank"
+naam = 19
+naam = true
+
+let umar = 20
+
+//dynamically typed
+
+console.log(naam)
+
+//synchronous
+
+console.log(umar)
+console.log(naam)
